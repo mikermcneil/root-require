0bcd453032c451428a830357238ebc321b8549a0
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-03 08:50:50 -0600

    Patch

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ var packpath = require('packpath');
 
 
 /**
- * Use `packpath` (https://github.com/jprichardson/node-packpath)
+ * Uses `packpath` (https://github.com/jprichardson/node-packpath)
  * to generate a more convenient require method.
  *
  * Not efficient (uses fs.*Sync() methods), but great for tests!
  * 
- * @type {[type]}
+ * @type {Function}
  */
-module.exports = function requireFromModuleRoot ( relativePathFromModuleRoot ) {
-	return require(path.join(packpath.self(),relativePathFromModuleRoot));
+module.exports = function requireFromModuleRoot ( __dirname ) {
+	return function (relativePathFromModuleRoot) {
+		return require(path.join(packpath.self(),relativePathFromModuleRoot));
+	};
 };
